cfaa919d9bb1e5f68b78b4ef9f79eda9e1f0b477
kajal mishra <111572290+mishrakajal2200@users.noreply.github.com>
2025-02-22 10:14:03 -0500

    changed code on 22

--- models/User.js
+++ models/User.js
@@ -1,18 +1,4 @@
 
-// const mongoose = require("mongoose");
-
-// const userSchema = new mongoose.Schema({
-//   username: String,
-//   email: { type: String, unique: true },
-//   password: String, // Hash this in the controller
-//   interests: [String], // User-selected interests at signup
-//   traits: [String], // Extracted traits after voice analysis
-//   socketId: String, // Store user's socket ID for real-time chat
-// });
-
-// module.exports = mongoose.model("User", userSchema);
-
-
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
